383a38500ea189faf3744a0d7f4b09e929881334
Jair Torezone <jairtorezone@gmail.com>
2022-09-26 22:18:56 -0300

    update project

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,18 @@
-document.getElementById("data").value = Date();
+document.getElementById("data").value = mostrarDataAtual();
+
+function mostrarDataAtual() {
+  let d = new Date();
+
+  //Date String
+  //let ds = new Date('2020-02-01 15:42:17');
+
+  let year = d.getFullYear();
+  let month = d.getMonth();
+  let day = d.getDate(); // dia atual
+
+  let hours = d.getHours();
+  let minuntes = d.getMinutes();
+  let second = d.getSeconds();
+
+  return `${day}/${month + 1}/${year} ${hours}:${minuntes}:${second}`;
+}
